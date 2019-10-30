2c9e70dd5ba6cd5cf0e524b5238c770b8b608aad
Mislav Milicevic <milicevic.mislav@nsoft.com>
2019-10-30 14:06:02 +0100

    Make resolver JWTProcessor instance modifiable

--- spring/resolver/src/main/java/com/nsoft/api/security/spring/resolver/JWTClaimsSetArgumentResolverSupport.java
+++ spring/resolver/src/main/java/com/nsoft/api/security/spring/resolver/JWTClaimsSetArgumentResolverSupport.java
@@ -5,6 +5,7 @@ import com.nsoft.api.security.jwt.verifier.JWTProcessor;
 import com.nsoft.api.security.spring.resolver.internal.util.HeaderUtil;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 /**
  * Support class which provides support for bearer token and {@link JWTClaimsSet} extraction.
@@ -14,17 +15,21 @@ import javax.servlet.http.HttpServletRequest;
  */
 public abstract class JWTClaimsSetArgumentResolverSupport {
 
-    private static final JWTProcessor PROCESSOR = JWTProcessor.getDefault();
-
     private static final Exception BEARER_TOKEN_EXCEPTION = new Exception(
             "Missing or invalid token");
     private static final Exception CLAIMS_SET_EXCEPTION = new Exception("Missing JWTClaimsSet");
 
     /**
+     * This {@link JWTProcessor} is used to process incoming Bearer tokens and construct {@link
+     * JWTClaimsSet} instances
+     */
+    private JWTProcessor processor = JWTProcessor.getDefault();
+
+    /**
      * Extracts a bearer token from an incoming HTTP request
      *
      * @param request incoming HTTP request
-     * @return bearer token
+     * @return Bearer token
      * @throws Exception if the bearer token is missing or invalid
      */
     protected String extractBearerToken(HttpServletRequest request) throws Exception {
@@ -35,12 +40,25 @@ public abstract class JWTClaimsSetArgumentResolverSupport {
     /**
      * Returns a {@link JWTClaimsSet} constructed from a Bearer token
      *
-     * @param bearerToken bearer token
+     * @param bearerToken Bearer token
      * @return {@link JWTClaimsSet} constructed from a Bearer token
      * @throws Exception if {@link JWTClaimsSet} is missing
      */
     protected JWTClaimsSet getClaimsSet(String bearerToken) throws Exception {
-        return PROCESSOR.process(bearerToken).orElseThrow(() -> CLAIMS_SET_EXCEPTION);
+        return processor.process(bearerToken).orElseThrow(() -> CLAIMS_SET_EXCEPTION);
+    }
+
+    /**
+     * Sets the {@link JWTProcessor} which will be used to process incoming Bearer tokens and
+     * construct {@link JWTClaimsSet} instances.
+     * <p>
+     * If the specified {@link JWTProcessor} is {@code null}, the processor gets set to the default
+     * implementation ({@link JWTProcessor#getDefault()})
+     *
+     * @param processor to use when processing incoming bearer tokens
+     */
+    public void setProcessor(JWTProcessor processor) {
+        this.processor = Objects.requireNonNullElse(processor, JWTProcessor.getDefault());
     }
 
 }
